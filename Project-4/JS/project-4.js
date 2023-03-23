8c1460a03983e99355fe4db70106ccb253499711
Sarthak <47793435+sarth99016@users.noreply.github.com>
2023-03-23 01:09:04 -0400

    Update project-4.js

--- Project-4/JS/project-4.js
+++ Project-4/JS/project-4.js
@@ -1,4 +1,4 @@
-//  Cariables
+//  Variables
 
 const addTask = document.getElementById('add-task')
 const taskContainer = document.getElementById('task-container')
@@ -33,12 +33,25 @@ addTask.addEventListener('click',function(){
 
     inputTask.value = ""
 
-    checkButton.addEventListener('click',function(){
+    checkButton.addEventListener('click',function(e){
+        let target = e.target
         checkButton.parentElement.style.textDecoration = "line-through"
+        console.log(e)
+        console.log(e.target)
+        console.log(target.parentElement)
+        console.log(target.parentElement.parentElement)
     });
 
-    deleteButton.addEventListener('click',function(e){
-        let target = e.target
-        target.parentElement.parentElement.remove()    
-    });
+    // when you click the trash can icon within the button, it will fail by deleting everything including the parent. The icon is its own DOM element and is NOT the button element.
+    deleteButton.addEventListener('click', function (e) {
+		let target = e.target;
+		if (target.matches('button')) {
+			target.parentElement.remove();
+            alert('button')
+		}
+		if (target.matches('i')) {
+			target.parentElement.parentElement.remove();
+		}
+        alert('i')
+	});
 });
